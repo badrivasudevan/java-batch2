9bddc4e8e8a06c447ab78f02160b7c304872452e
Elton.Lim <Elton.Lim@LAPBTN12367.fdmgroup.local>
2016-10-27 10:44:16 +0800

    orderprocess

---
+++ src/com/fdm/wealthnow/common/OrderResultProcessor.java
@@ -0,0 +1,26 @@
+package src.com.fdm.wealthnow.common;
+
+import src.com.fdm.wealthnow.common.Logger;
+import src.com.fdm.wealthnow.common.Order;
+import src.com.fdm.wealthnow.common.OrderStatus;
+
+public class OrderResultProcessor {
+	private final Logger logger;
+
+	public OrderResultProcessor(Logger logger){
+		this.logger = logger;
+	}
+
+	public void update(Order order){
+
+		if(order.getStatus() == OrderStatus.Processed){
+			logger.log(order.orderId + " is processed");
+		}
+		if(order.getStatus() == OrderStatus.Pending){
+			logger.log(order.orderId + " is pending");
+		}
+		logger.log(order.orderId + " is unsuccessful");
+
+	}
+
+}
