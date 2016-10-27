7d71b2267b9b05fdbda1f9ba8d56479fad38a58b
Elton.Lim <Elton.Lim@LAPBTN12367.fdmgroup.local>
2016-10-27 14:08:56 +0800

    holdings class

---
+++ src/com/fdm/wealthnow/common/Holdings.java
@@ -0,0 +1,54 @@
+package com.fdm.wealthnow.common;
+
+public class Holdings {
+	private long holdingsId;
+	private long userId;
+	private String stockSymbol;
+	private int remainingQuantity;
+	private double pricePaid;
+	private String currency;
+
+
+	public Holdings(long holdingsId, long userId, String stockSymbol, int remainingQuantity,
+			double pricePaid, String currency) {
+		this.holdingsId = holdingsId;
+		this.userId = userId;
+		this.stockSymbol = stockSymbol;
+		this.remainingQuantity = remainingQuantity;
+		this.pricePaid = pricePaid;
+		this.currency = currency;
+	}
+
+
+	public long getHoldingsId() {
+		return holdingsId;
+	}
+
+
+	public long getUserId() {
+		return userId;
+	}
+
+
+	public String getStockSymbol() {
+		return stockSymbol;
+	}
+
+
+	public int getRemainingQuantity() {
+		return remainingQuantity;
+	}
+
+
+	public double getPricePaid() {
+		return pricePaid;
+	}
+
+
+	public String getCurrency() {
+		return currency;
+	}
+	
+	
+
+}
