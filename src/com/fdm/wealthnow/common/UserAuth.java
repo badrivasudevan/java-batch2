637c3eff4c02751dab5e6fdba2a90ab47a444711
Badri Vasudevan <vbadri@gmail.com>
2016-10-27 10:30:00 +0800

    User auth files

---
+++ src/com/fdm/wealthnow/common/UserAuth.java
@@ -0,0 +1,47 @@
+package com.fdm.wealthnow.common;
+
+public class UserAuth {
+	boolean authenticationSuccess = false;
+	User user;
+	String errorMsg;
+	
+	public UserAuth() {
+		
+	}
+	public UserAuth(boolean authenticationStatus) {
+		this.authenticationSuccess = authenticationStatus;
+	}
+	public UserAuth(User user, boolean authenticationStatus) {
+		this.user = user;
+		this.authenticationSuccess = authenticationStatus;
+	}
+	
+	public UserAuth(boolean authenticationSuccess, User user, String errorMsg) {
+		this.authenticationSuccess = authenticationSuccess;
+		this.user = user;
+		this.errorMsg = errorMsg;
+	}
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+	public void setAuthenticationSuccess(boolean authenticationSuccess) {
+		this.authenticationSuccess = authenticationSuccess;
+	}
+
+	public boolean isAuthenticationSuccess() {
+		return authenticationSuccess;
+	}
+
+}
