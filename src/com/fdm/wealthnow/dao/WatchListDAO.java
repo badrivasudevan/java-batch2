c02b9ba01091ea328fe0041225233809612fe068
LingShih.Chen <LingShih.Chen@LAPBTN11950.fdmgroup.local>
2016-10-27 14:50:30 +0800

    yo

--- src/com/fdm/wealthnow/dao/WatchListDAO.java
+++ src/com/fdm/wealthnow/dao/WatchListDAO.java
@@ -4,5 +4,5 @@ public class WatchListDAO {
 
 	//Activate connection to SQL database where the watchlist_table is stored 
 	//extract information and update the information based on the method being called in java
-	
+	//
 }
