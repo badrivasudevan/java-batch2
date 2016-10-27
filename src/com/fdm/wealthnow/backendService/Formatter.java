244e16b8528a9a76acedec5d1411a56777e502a2
Elton.Lim <Elton.Lim@LAPBTN12367.fdmgroup.local>
2016-10-27 14:28:24 +0800

    formatting

--- src/com/fdm/wealthnow/backendService/Formatter.java
+++ src/com/fdm/wealthnow/backendService/Formatter.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.fdm.wealthnow.common.OrderStatus;
+import com.fdm.wealthnow.common.PriceType;
 import com.fdm.wealthnow.common.Term;
 import com.fdm.wealthnow.common.TransactionType;
 
@@ -43,5 +45,29 @@ public class Formatter {
 			return Term.GoodUntilCancelled;
 		}
 	}
+	
+	public static OrderStatus formatOrderStatus (String orderStatus){ 
 
+		if (orderStatus == "Pending")
+		{
+			return OrderStatus.Pending;
+		}
+		else if (orderStatus == "Completed"){
+			return OrderStatus.Completed;
+		}
+		else {
+			return OrderStatus.Cancelled;
+		}
+	}
+	
+	public static PriceType formatTransactionType (String priceType){
+		if (priceType == "Market"){
+			return PriceType.Market;
+		}
+		else if (priceType == "Limit"){
+			return PriceType.Limit;
+		}
+		return PriceType.StopLoss;
+
+	}
 }
