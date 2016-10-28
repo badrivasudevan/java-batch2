076754b3478c9512e9d05b3459bfd80bafc94804
Oliver.Cheok <Oliver.Cheok@LAPBTN11900.fdmgroup.local>
2016-10-28 14:55:18 +0800

    updated orderprocessor

--- src/com/fdm/wealthnow/backendService/OrderProcessor.java
+++ src/com/fdm/wealthnow/backendService/OrderProcessor.java
@@ -6,6 +6,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +16,7 @@ import javax.servlet.http.HttpSession;
 import javax.swing.text.DateFormatter;
 
 import com.fdm.wealthnow.common.OrderStatus;
-import com.fdm.wealthnow.common.Orders;
+import com.fdm.wealthnow.common.Order;
 import com.fdm.wealthnow.common.PriceType;
 import com.fdm.wealthnow.common.Term;
 import com.fdm.wealthnow.common.TransactionType;
@@ -40,10 +41,48 @@ public class OrderProcessor extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+//      session.setAttribute("loggedInUser", user);
+		User currentUser = (User) (session.getAttribute("loggedInUser"));
+		
+		long userID =  currentUser.getUserId();
+		TransactionType transacType = Formatter.formatTransacType(request.getParameter("transactionType"));
+		
+		String stockSymbol = request.getParameter("symbol");
+		Term term = Formatter.formatTerm(request.getParameter("term")); 
+		PriceType priceType = Formatter.formatPriceType(request.getParameter("priceType"));
+		String limitBuy = request.getParameter("limitBuy");
+		String limitSell = request.getParameter("limitSell");
+		String stopLoss = request.getParameter("stopLoss");
+		double priceExecuted = 0;
+		int orderQuantity = Integer.parseInt(request.getParameter("quantity"));
+		
+		if (limitBuy != null) {
+			priceExecuted = Double.parseDouble(limitBuy);
+			
+		}
+		
+		if (limitSell != null) {
+			priceExecuted = Double.parseDouble(limitSell);
+			
+		}
+		
+		if (stopLoss != null) {
+			priceExecuted = Double.parseDouble(stopLoss);
+			
+		}
 		
 		
+		//double priceExecuted = Double.parseDouble(request.getParameter("priceExecuted"));
+		OrderStatus orderStatus = OrderStatus.Pending;
+		
+		Order newOrder = new Order(userID, transacType, orderQuantity, stockSymbol, term, priceType, priceExecuted, orderStatus);
+		
+		request.setAttribute("fieldList", newOrder);
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/OrderFormConfirmation.jsp");
+		dispatcher.forward(request, response);
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
 		
 	}
@@ -52,8 +91,8 @@ public class OrderProcessor extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		
 	}
 	
 	private long createOrder(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -93,7 +132,7 @@ public class OrderProcessor extends HttpServlet {
 		//double priceExecuted = Double.parseDouble(request.getParameter("priceExecuted"));
 		OrderStatus orderStatus = OrderStatus.Pending;
 		
-		Orders newOrder = new Orders(userID, transacType, orderQuantity, stockSymbol, term, priceType, priceExecuted, orderStatus);
+		Order newOrder = new Order(userID, transacType, orderQuantity, stockSymbol, term, priceType, priceExecuted, orderStatus);
 		
 		return newOrder.getOrderID();
 	}
