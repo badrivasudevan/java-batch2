255adfbe7588c88040c09c1c275d6f0c71b27ca9
Chen Ruo <chen.ruo@hotmail.com>
2016-10-31 10:26:07 +0800

    Update BuySellTask

--- src/com/fdm/wealthnow/backendService/BuySellTask.java
+++ src/com/fdm/wealthnow/backendService/BuySellTask.java
@@ -1,7 +1,11 @@
 package com.fdm.wealthnow.backendService;
 
 import com.fdm.wealthnow.common.Order;
+import com.fdm.wealthnow.common.OrderStatus;
 import com.fdm.wealthnow.common.StockService;
+import com.fdm.wealthnow.common.TransactionType;
+
+import commonObject.StockData;
 
 public class BuySellTask implements Runnable {
 	
@@ -18,8 +22,27 @@ public class BuySellTask implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 		
+		TransactionType type = order.getTransacType();
 		String stockSymbol = order.getStockSymbol();
+		double price;
 		
+		if(type == TransactionType.Buy){
+			
+			price = order.getPriceExecuted();
+			
+		}
+		else{
+			
+		}	
+		
+	}
+	
+	private void BuyTask(Order order){
+		
+		if(OrderService.validateCashBalance(order)){
+			
+			order.setOrderStatus(OrderStatus.Completed);
+		}
 		
 	}
 	
