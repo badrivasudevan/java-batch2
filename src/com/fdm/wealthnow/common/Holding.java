07c71277ce0aeb1ed9693f73f36b5e988f235d11
Michelle.Chia <Michelle.Chia@LAPBTN12793.fdmgroup.local>
2016-11-03 11:29:03 +0800

    holding updated

--- src/com/fdm/wealthnow/common/Holding.java
+++ src/com/fdm/wealthnow/common/Holding.java
@@ -7,26 +7,48 @@ public class Holding {
 	private int remainingQuantity;
 	private double pricePaid;
 	private String currency;
+	private double currentStockWorth;
+	private double moneyRealized;
+	private double profitLoss;
 
 	public Holding(long userId, String stockSymbol, int remainingQuantity,
-			double pricePaid, String currency) {
+			double pricePaid, String currency, double currentStockWorth, double moneyRealized, double profitLoss) {
 		this.userId = userId;
 		this.stockSymbol = stockSymbol;
 		this.remainingQuantity = remainingQuantity;
 		this.pricePaid = pricePaid;
 		this.currency = currency;
+		this.currentStockWorth = currentStockWorth;
+		this.moneyRealized = moneyRealized;
+		this.profitLoss = profitLoss;
 	}
 	
 	public Holding(long holdingsId, long userId, String stockSymbol, int remainingQuantity,
-			double pricePaid, String currency) {
+			double pricePaid, String currency, double currentStockWorth, double moneyRealized, double profitLoss) {
 		this.holdingsId = holdingsId;
 		this.userId = userId;
 		this.stockSymbol = stockSymbol;
 		this.remainingQuantity = remainingQuantity;
 		this.pricePaid = pricePaid;
 		this.currency = currency;
+		this.currentStockWorth = currentStockWorth;
+		this.moneyRealized = moneyRealized;
+		this.profitLoss = profitLoss;
 	}
 
+	
+	
+	public double getCurrentStockWorth() {
+		return currentStockWorth;
+	}
+
+	public double getMoneyRealized() {
+		return moneyRealized;
+	}
+
+	public double getProfitLoss() {
+		return profitLoss;
+	}
 
 	public long getHoldingsId() {
 		return holdingsId;
@@ -55,6 +77,30 @@ public class Holding {
 
 	public String getCurrency() {
 		return currency;
+	}	
+	
+	public void setHoldingsId(long holdingsId) {
+		this.holdingsId = holdingsId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public void setStockSymbol(String stockSymbol) {
+		this.stockSymbol = stockSymbol;
+	}
+
+	public void setCurrentStockWorth(double currentStockWorth) {
+		this.currentStockWorth = currentStockWorth;
+	}
+
+	public void setMoneyRealized(double moneyRealized) {
+		this.moneyRealized = moneyRealized;
+	}
+
+	public void setProfitLoss(double profitLoss) {
+		this.profitLoss = profitLoss;
 	}
 
 	public void setRemainingQuantity(int remainingQuantity) {
