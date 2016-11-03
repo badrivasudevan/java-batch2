cf23531166e8f3d0ec9bfcb8105a2df156a3c6f3
LingShih.Chen <LingShih.Chen@LAPBTN11950.fdmgroup.local>
2016-11-03 11:18:21 +0800

    yo

--- src/com/fdm/wealthnow/controller/RegistrationController.java
+++ src/com/fdm/wealthnow/controller/RegistrationController.java
@@ -2,13 +2,15 @@ package com.fdm.wealthnow.controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.fdm.wealthnow.dao.WatchListDAO;
+import com.fdm.wealthnow.dao.UserDAO;
 
 public class RegistrationController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -41,17 +43,38 @@ public class RegistrationController extends HttpServlet {
 		String password2 = request.getParameter("confirmpassword");
 		String user_errormsg = "The passwords do not match!";
 		String user_errormsg2 = "The username is already in use. Please use other username!";
-		
-		if(password1.equals(password2)) {			
-		}
-		
-		else {
-			 System.out.println("The passwords do not match! Try again!");
-			 request.setAttribute("errorMessage", user_errormsg);
-			 request.getRequestDispatcher("/registration.jsp").forward(request, response);
+		String user_register = "The account is created! You can now access the website now!";
+		List<String> userlist = new ArrayList<>();
+		try {
+			userlist = UserDAO.retrieveAllUserName();
+			if(userlist.contains(username)){
+				 System.out.println("The username is already exist! Try again!");
+				 request.setAttribute("errorMessage2", user_errormsg2);
+				 request.getRequestDispatcher("/registration.jsp").forward(request, response);				
+			}
+			else {
+				if(password1.equals(password2)) {		
+					 try {
+						UserDAO.newUser(username, fullname, password1);
+						System.out.println("The username is already exist! Try again!");
+						request.setAttribute("success", user_register);
+						request.getRequestDispatcher("/registration.jsp").forward(request, response);				
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				else {
+					 System.out.println("The passwords do not match! Try again!");
+					 request.setAttribute("errorMessage", user_errormsg);
+					 request.getRequestDispatcher("/registration.jsp").forward(request, response);
+				}			
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	
-
 	}
 
 }
