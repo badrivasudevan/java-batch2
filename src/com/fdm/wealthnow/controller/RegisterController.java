fb131dde7f7dcbdb674e4ab2f6f0f58540b16131
LingShih.Chen <LingShih.Chen@LAPBTN11950.fdmgroup.local>
2016-11-03 16:45:40 +0800

    yo

--- src/com/fdm/wealthnow/controller/RegisterController.java
+++ src/com/fdm/wealthnow/controller/RegisterController.java
@@ -43,6 +43,7 @@ public class RegisterController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//doGet(request, response);
+		
 
 		String username = request.getParameter("username");
 		String fullname = request.getParameter("fullname");
