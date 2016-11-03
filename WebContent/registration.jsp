9be49b18f5c6145085d62346e5034907eb97acbd
LingShih.Chen <LingShih.Chen@LAPBTN11950.fdmgroup.local>
2016-11-03 10:48:47 +0800

    yo

---
+++ WebContent/registration.jsp
@@ -0,0 +1,51 @@
+<%@ page 
+	import="com.fdm.wealthnow.common.User" %>
+ <%@ page 
+	import="com.fdm.wealthnow.common.Stock" %>
+ <%@ page 
+	import="com.fdm.wealthnow.common.StockService" %>	
+ <%@ page 
+ 	import="com.fdm.wealthnow.dao.WatchListDAO" %>	
+ <%@ page import="com.fdm.wealthnow.dao.UserDAO"%>
+<html>
+<head>
+	<style>
+	
+	</style>
+</head>
+<body>
+	<form action="${pageContext.request.contextPath}/RegistrationController" method="POST">	
+	Username : <input type="text" name="username" /> <br>
+	Full name : <input type="text" name="fullname" /> <br>
+	Password : <input type="password" name="password" /> <br>
+	Confirm Password : <input type="confirmpassword" name="confirmpassword" /> <br>
+		
+	<fieldset>
+		<%	if(request.getAttribute("errorMessage") != null) {	 %>
+	  		     <!-- out.println(request.getAttribute("errorMessage")); -->
+	  		   <script>alert("No such watchlist in the database!");</script>
+	    <%		}
+		%>
+		<%	if(request.getAttribute("removewatchlist") != null) {	 %>
+	  		     <!-- out.println(request.getAttribute("errorMessage")); -->
+	  		   <script>alert("removewatchlist");</script>
+	    <%		}
+		%>
+		<%	if(request.getAttribute("errorMessage2") != null) {	 %>
+	  		     <!-- out.println(request.getAttribute("errorMessage")); -->
+	  		   <script>alert("The watchlist is already in the database!");</script>
+	    <%		}
+		%>
+		<%	if(request.getAttribute("addwatchlist") != null) {	 %>
+	  		     <!-- out.println(request.getAttribute("errorMessage")); -->
+	  		   <script>alert("addwatchlist");</script>
+	    <%		}
+		%>
+
+		<button type="submit">Submit</button> <br><br>
+		</form>
+		</div> <br>
+	</fieldset>
+	
+</body>
+</html>
--- src/com/fdm/wealthnow/controller/RegistrationController.java
+++ src/com/fdm/wealthnow/controller/RegistrationController.java
@@ -1,5 +1,60 @@
 package com.fdm.wealthnow.controller;
 
-public class RegistrationController {
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fdm.wealthnow.dao.WatchListDAO;
+
+public class RegistrationController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistrationController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		long userId = Long.parseLong(request.getParameter("userid"));
+		String allwatchlistforuser = request.getParameter("Watchlist");
+		String w_id = "";
+		
+		try {
+			for(String s : WatchListDAO.retrieveWatchlist(userId).keySet()){
+				 if(WatchListDAO.retrieveWatchlist(userId).get(s).equals(allwatchlistforuser)){
+						w_id = s;
+						System.out.println("watchlistid :" +w_id);
+						request.setAttribute("watchlistid", w_id);
+						break;
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		request.getRequestDispatcher("/WatchList.jsp").forward(request, response);
+
+	}
 
 }
+
